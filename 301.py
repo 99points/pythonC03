0e88d045c99667c7dcd0e9a19ece29fd22701bb1
Zeeshan <mrzeepk@gmail.com>
2022-12-08 16:03:43 +0500

    final commit for 301

--- 301.py
+++ 301.py
@@ -1,4 +1,14 @@
-# your code goes here
+# 3.1
+# 3.1 Drivers are concerned with the mileage obtained by their automobiles. One driver has kept track of several tankfuls of gasoline by recording miles driven and gallons used for each tankful. Develop a Python program that prompts the user to input the miles driven and gallons used for each tank- ful. The program should calculate and display the miles per gallon obtained for each tankful. After processing all input information, the program should calculate and print the combined miles per gal- lon obtained for all tankful (= total miles driven divided by total gallons used).
+# Enter the gallons used (-1 to end): 12.8
+# Enter the miles driven: 287
+# The miles / gallon for this tank was 22.421875 Enter the gallons used (-1 to end): 10.3
+# Enter the miles driven: 200
+# The miles / gallon for this tank was 19.417475 Enter the gallons used (-1 to end): 5
+# Enter the miles driven: 120
+# The miles / gallon for this tank was 24.000000 Enter the gallons used (-1 to end): -1
+# The overall average miles/gallon was 21.601423
+
 gallonUsed = 0
 gallonUsedTotal = 0
 miles = 0
